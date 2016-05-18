d7927ff762892787cd73a3f9965d726648282013
memobelt <guillermo_beltran@brown.edu>
2016-05-18 23:23:32 +0200

    Fragment Manager :D

---
+++ app/src/main/java/aitfinalproject/wakeup/Constants.java
@@ -0,0 +1,8 @@
+package aitfinalproject.wakeup;
+
+/**
+ * Created by Memo on 5/18/16.
+ */
+public class Constants {
+    public static String LAYOUT = "LAYOUT";
+}
--- app/src/main/java/aitfinalproject/wakeup/MainActivity.java
+++ app/src/main/java/aitfinalproject/wakeup/MainActivity.java
@@ -1,24 +1,25 @@
 package aitfinalproject.wakeup;
 
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import aitfinalproject.wakeup.Fragments.ManageFragments;
+
+public class MainActivity extends AppCompatActivity implements ManageFragments.OnFragmentInteractionListener{
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -85,7 +86,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
+    }
 
 
     /**
@@ -102,7 +106,19 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            Bundle args = new Bundle();
+            ManageFragments mf = new ManageFragments();
+            if (position == 0) {
+                args.putInt(Constants.LAYOUT,R.layout.fragment_alarm_clock);
+            }
+            if (position == 1) {
+                args.putInt(Constants.LAYOUT,R.layout.fragment_stop_watch);
+            }
+            if (position == 2) {
+                args.putInt(Constants.LAYOUT,R.layout.fragment_timer);
+            }
+            mf.setArguments(args);
+            return mf;
         }
 
         @Override
